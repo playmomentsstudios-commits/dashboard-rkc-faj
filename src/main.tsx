c395df5c02c6e9bb0ad3a66e5f75312e7330d6ed
Felipe Costa <playmomentsstudios@gmail.com>
2026-05-29 18:23:01 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/faj">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
